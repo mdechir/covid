4e8da9ab1b0c60ccc02b6e95bc5c6261a8141ff5
mdechir <mdechir@la-providence.net>
2021-03-23 15:31:10 +0100

    add PDO

--- index.php
+++ index.php
@@ -1,1 +1,9 @@
-<?php echo "coucou" ?>
+<?php
+   try{
+      $pdo=new PDO("mysql:host=mysql-dechirmathys.alwaysdata.net ;dbname=dechirmathys_virus","229073","TEST123Belddfsd");
+      echo "coucou";
+   }
+   catch(PDOException $e){
+      echo $e->getMessage();
+   }
+?>
